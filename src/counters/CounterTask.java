0f2f5c9495cefee155991fb6ddefa2ad217f4278
Eng. Duraid <Duraid.hadaya@gmail.com>
2026-03-19 00:00:41 +0300

    split multi threads to be 4
    Implemented a fixed 4-thread solution (t1–t4). The directory contents are divided into four equal parts, and each thread processes a portion independently. Each thread counts PDF files in its assigned range, and the results are combined after all threads complete using join().

--- src/counters/CounterTask.java
+++ src/counters/CounterTask.java
@@ -1,5 +1,38 @@
 package counters;
 
-public class CounterTask {
+import java.io.File;
+import scanner.FileScanner;
 
+public class CounterTask implements Runnable {
+
+    private File[] files;
+    private int start;
+    private int end;
+    private int count = 0;
+
+    public CounterTask(File[] files, int start, int end) {
+        this.files = files;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = start; i < end; i++) {
+
+            File file = files[i];
+
+            if (file.isDirectory()) {
+                count += FileScanner.countPDF(file);
+            } 
+            else if (file.getName().toLowerCase().endsWith(".pdf")) {
+                count++;
+            }
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
--- src/counters/MultiThreadCounter.java
+++ src/counters/MultiThreadCounter.java
@@ -1,6 +1,7 @@
 package counters;
 
 import java.io.File;
+import scanner.FileScanner;
 
 public class MultiThreadCounter extends Thread {
 
@@ -17,25 +18,39 @@ public class MultiThreadCounter extends Thread {
         File[] files = dir.listFiles();
         if (files == null) return;
 
-        Thread[] threads = new Thread[files.length];
-        CounterTask[] tasks = new CounterTask[files.length];
+        int length = files.length;
 
-     
-        for (int i = 0; i < files.length; i++) {
-            tasks[i] = new CounterTask(files[i]);
-            threads[i] = new Thread(tasks[i]);
-            threads[i].start();
+        int partSize = length / 4;
+
+        CounterTask t1Task = new CounterTask(files, 0, partSize);
+        CounterTask t2Task = new CounterTask(files, partSize, partSize * 2);
+        CounterTask t3Task = new CounterTask(files, partSize * 2, partSize * 3);
+        CounterTask t4Task = new CounterTask(files, partSize * 3, length);
+
+        Thread t1 = new Thread(t1Task);
+        Thread t2 = new Thread(t2Task);
+        Thread t3 = new Thread(t3Task);
+        Thread t4 = new Thread(t4Task);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-       
-        for (int i = 0; i < threads.length; i++) {
-            try {
-                threads[i].join();
-                count += tasks[i].getCount();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+      
+        count = t1Task.getCount() +
+                t2Task.getCount() +
+                t3Task.getCount() +
+                t4Task.getCount();
     }
 
     public int getCount() {
