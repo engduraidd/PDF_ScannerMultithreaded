25591e0838d1ba20df8802604956f67f8aaaea65
Eng. Duraid <Duraid.hadaya@gmail.com>
2026-03-18 13:09:15 +0300

    Design the structure of the project
    Add and named needed packages and class according to the assignment requirement . including information about group students and student information

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -1,9 +1,24 @@
+
 package main;
+import java.io.File;
+import java.util.Scanner;
 
-public class MainApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class MainApp 
+{
+
+	void main(String[] args) 
+	
+	
+	{
+		/**********************************StudentInfos**********************************
+		Group Name: ThreadX2
+		Prepared By: -> Duraid Haytham Shaba		ID: dh21290
+		 			 -> Basoz Mohammed Ali			ID: bm22142
+		 Course: SE422-S2
+		 University: American University of Iraq (AUIS)
+		 *******************************************************************************/
+	
 
 	}
 
